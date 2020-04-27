795192f1a930bbe05e1820e0795dcac188cfdb27
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2020-04-26 20:02:17 -0500

    post request test 1

--- recycle/lib/home_page.dart
+++ recycle/lib/home_page.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:recycle/login_page.dart';
 import 'package:recycle/sign_in.dart';
 import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'package:http_parser/http_parser.dart';
 
 class HomePage extends StatefulWidget{
   HomePage({Key key, this.title}) : super(key: key);
@@ -16,6 +22,25 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  Upload(File imageFile) async {
+    var stream = new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+    var length = await imageFile.length();
+
+    var uri = Uri.parse("capstone.murtazahakimi.com/predict");
+
+    var request = new http.MultipartRequest("POST", uri);
+    var multipartFile = new http.MultipartFile('file', stream, length,
+        filename: basename(imageFile.path));
+    //contentType: new MediaType('image', 'png'));
+
+    request.files.add(multipartFile);
+    var response = await request.send();
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+    });
+    print(response.statusCode);
+  }
+
   /* this is the function that handles getting the image from the device*/
   File _image;
   Future _getImage() async {
@@ -23,7 +48,8 @@ class _HomePageState extends State<HomePage>{
     ImageSource.camera);
 
     setState(() {
-      _image = image;}
+      _image = image;
+      Upload(_image); }
     );
   }
 
